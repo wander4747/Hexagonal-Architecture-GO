d77d8380567ba89f41630b42e7a15d2a224e6e37
wander4747 <wander.douglas14@gmail.com>
2021-05-29 15:07:40 -0300

    implementing simple methods

--- application/product.go
+++ application/product.go
@@ -1,1 +1,51 @@
 package application
+
+type ProductInterface interface {
+	IsValid() (bool, error)
+	Enable() error
+	Disable() error
+	GetID() string
+	GetName() string
+	GetStatus() string
+	GetPrice() float64
+}
+
+const (
+	DISABLED = "disabled"
+	ENABLED  = "enabled"
+)
+
+type Product struct {
+	ID     string
+	Name   string
+	Price  float64
+	Status string
+}
+
+func (p *Product) IsValid() (bool, error) {
+	return true, nil
+}
+
+func (p *Product) Enable() error {
+	return nil
+}
+
+func (p *Product) Disable() error {
+	return nil
+}
+
+func (p *Product) GetID() string {
+	return p.ID
+}
+
+func (p *Product) GetName() string {
+	return p.Name
+}
+
+func (p *Product) GetStatus() string {
+	return p.Status
+}
+
+func (p *Product) GetPrice() float64 {
+	return p.Price
+}
