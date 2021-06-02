5035c39d4a7fdd6bc32348b410119811dba797d0
wander4747 <wander.douglas14@gmail.com>
2021-06-02 18:19:18 -0300

    create adapter web

---
+++ adapters/web/handler/product.go
@@ -0,0 +1,34 @@
+package handler
+
+import (
+	"encoding/json"
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	"github.com/wander4747/go-hexagonal/application"
+	"net/http"
+)
+
+func MakeProductHandlers(r *mux.Router, n *negroni.Negroni, service application.ProductServiceInterface) {
+	r.Handle("/product/{id}", n.With(
+		negroni.Wrap(getProduct(service)),
+	)).Methods(http.MethodGet, http.MethodOptions)
+}
+
+func getProduct(service application.ProductServiceInterface) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		vars := mux.Vars(r)
+		id := vars["id"]
+
+		product, err := service.Get(id)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		err = json.NewEncoder(w).Encode(product)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	})
+}
